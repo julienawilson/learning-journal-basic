da71e26732493f0287df91bbd4ce9d70a2f1d76d
Julien Wilson <julienawilson@gmail.com>
2016-12-20 21:20:37 -0800

    templates for all pages

--- learning_journal_basic/tests.py
+++ learning_journal_basic/tests.py
@@ -12,5 +12,5 @@ def test_home_page_renders_file_data(req):
     """My home page view returns some data."""
     from .views import list_view
     response = list_view(req)
-    some_html = '<a class="navbar-brand" href="#">Julien Wilson</a>'
-    assert some_html in response
+    some_html = 'Day 11 - Heroku seemed easier last time'
+    assert some_html in str(response)
--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -7,7 +7,7 @@ THIS_DIR = os.path.dirname(__file__)
 
 
 ENTRIES = {
-    '11': {"title":"Day 11 - Heroku seemed easier last time",
+    '11': {"title": "Day 11 - Heroku seemed easier last time",
      "id": 11,
      "date": "December 19, 2016 AD",
      "body": "My main accomplishment today was debugging Heroku. My initial push (and my second attempt) failed, but I was able to read the errror messages and figure out what was going on.  I moved some files around and was able to make it work.  Generally, one of the things I've learned the most about in this class is reading error messages and figuring out how to fix them. Besides that, we presented ideas today.  Despite almost nobody having any ideas 5 minutes before the meeting, there were A LOT of great ideas.  I think people are thinking big.  Would be excited to work on a number of them. Data Structures are starting to feel more natural.  The Deque wasnt too tough today.  I've heard the heap is a little more exciting.  We'll see what that brings tomorrow."},
@@ -31,24 +31,24 @@ def list_view(request):
     return {"ENTRIES": ENTRIES}
 
 
-@view_config(route_name="edit", renderer="string")
+@view_config(route_name="edit", renderer="templates/update_post_template.jinja2")
 def update(request):
     """View """
-    imported_file = open(os.path.join(THIS_DIR, 'static', 'edit_article.html')).read()
-    # return Response(imported_file)
-    return imported_file
+    the_id = request.matchdict["id"]
+    entry = ENTRIES[the_id]
+    return {"entry": entry}
 
 
-@view_config(route_name="blog", renderer="string")
+@view_config(route_name="blog", renderer="templates/detail.jinja2")
 def detail(request):
-    imported_file = open(os.path.join(THIS_DIR, 'static', 'article.html')).read()
-    return Response(imported_file)
+    the_id = request.matchdict["id"]
+    entry = ENTRIES[the_id]
+    return {"entry": entry}
 
 
-@view_config(route_name="new", renderer="string")
+@view_config(route_name="new", renderer="templates/new_post_template.jinja2")
 def create(request):
-    imported_file = open(os.path.join(THIS_DIR, 'static', 'new_article.html')).read()
-    return Response(imported_file)
+    return {"ENTRIES": ENTRIES}
 
 
 # def includeme(config):
